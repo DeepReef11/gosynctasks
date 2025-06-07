e5f139aa1f05d46eb3a501dadbc4021e79491373
ok <ok@ok>
2025-06-06 22:44:55 -0400

    refactor: main

--- cmd/gosynctasks/main.go
+++ cmd/gosynctasks/main.go
@@ -13,7 +13,30 @@ import (
     _ "modernc.org/sqlite"
 )
 
-var taskLists []backend.TaskList
+type App struct {
+    taskLists   []backend.TaskList
+    taskManager backend.TaskManager
+    config      *config.Config
+}
+
+func NewApp() (*App, error) {
+    cfg := config.GetConfig()
+    taskManager, err := cfg.Connector.TaskManager()
+    if err != nil {
+        return nil, err
+    }
+
+    app := &App{
+        config:      cfg,
+        taskManager: taskManager,
+    }
+
+    if err := app.loadTaskLists(); err != nil {
+        log.Printf("Warning: Could not load task lists: %v", err)
+    }
+
+    return app, nil
+}
 
 func initDB() (*sql.DB, error) {
     db, err := sql.Open("sqlite", "tasks.db")
@@ -33,20 +56,16 @@ func initDB() (*sql.DB, error) {
     return db, err
 }
 
-func loadTaskLists() error {
-    config := config.GetConfig()
-    taskManager, err := config.Connector.TaskManager()
-    if err != nil {
-        return err
-    }
 
-    taskLists, err = taskManager.GetTaskLists()
+func (a *App) loadTaskLists() error {
+    var err error
+    a.taskLists, err = a.taskManager.GetTaskLists()
     return err
 }
 
-func listNameCompletion(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+func (a *App) listNameCompletion(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
     var completions []string
-    for _, list := range taskLists {
+    for _, list := range a.taskLists {
         if strings.HasPrefix(strings.ToLower(list.Name), strings.ToLower(toComplete)) {
             completions = append(completions, list.Name)
         }
@@ -54,8 +73,8 @@ func listNameCompletion(cmd *cobra.Command, args []string, toComplete string) ([
     return completions, cobra.ShellCompDirectiveNoFileComp
 }
 
-func findListByName(name string) *backend.TaskList {
-    for _, list := range taskLists {
+func (a *App) findListByName(name string) *backend.TaskList {
+    for _, list := range a.taskLists {
         if strings.EqualFold(list.Name, name) {
             return &list
         }
@@ -63,8 +82,8 @@ func findListByName(name string) *backend.TaskList {
     return nil
 }
 
-func cli_ListSelection(lists *[]backend.TaskList) (*backend.TaskList, error) {
-    for i, list := range *lists {
+func (a *App) selectListInteractively() (*backend.TaskList, error) {
+    for i, list := range a.taskLists {
         desc := ""
         if list.Description != "" {
             desc = " - " + list.Description
@@ -72,123 +91,91 @@ func cli_ListSelection(lists *[]backend.TaskList) (*backend.TaskList, error) {
         fmt.Printf("%d: %s%s\n", i+1, list.Name, desc)
     }
 
-    fmt.Printf("Enter selection (1-%d):\n", len(*lists))
+    fmt.Printf("Enter selection (1-%d): ", len(a.taskLists))
     var choice int
-    _, err := fmt.Scanf("%d", &choice)
-    if err != nil || choice < 1 || choice > len(*lists) {
+    if _, err := fmt.Scanf("%d", &choice); err != nil || choice < 1 || choice > len(a.taskLists) {
         return nil, fmt.Errorf("invalid choice: %d", choice)
     }
-    return &(*lists)[choice-1], err
+    return &a.taskLists[choice-1], nil
+}
+
+
+func (a *App) buildFilter(cmd *cobra.Command) *backend.TaskFilter {
+    filter := &backend.TaskFilter{}
+    var statuses []string
+
+    if done, _ := cmd.Flags().GetBool("done"); done {
+        statuses = append(statuses, "DONE")
+    }
+    if todo, _ := cmd.Flags().GetBool("todo"); todo {
+        statuses = append(statuses, "TODO")
+    }
+    if processing, _ := cmd.Flags().GetBool("processing"); processing {
+        statuses = append(statuses, "PROCESSING")
+    }
+    if cancelled, _ := cmd.Flags().GetBool("cancelled"); cancelled {
+        statuses = append(statuses, "CANCELLED")
+    }
+
+    if len(statuses) > 0 {
+        filter.Statuses = &statuses
+    }
+    return filter
+}
+
+func (a *App) run(cmd *cobra.Command, args []string) error {
+    var listName string
+    if len(args) > 0 {
+        listName = args[0]
+    }
+
+    filter := a.buildFilter(cmd)
+
+    var selectedList *backend.TaskList
+    var err error
+
+    if listName != "" {
+        selectedList = a.findListByName(listName)
+        if selectedList == nil {
+            return fmt.Errorf("list '%s' not found", listName)
+        }
+    } else {
+        selectedList, err = a.selectListInteractively()
+        if err != nil {
+            return err
+        }
+    }
+
+    tasks, err := a.taskManager.GetTasks(selectedList.ID, filter)
+    if err != nil {
+        return fmt.Errorf("error retrieving tasks: %w", err)
+    }
+
+    fmt.Println(selectedList)
+    fmt.Println(tasks)
+    return nil
 }
 
 func main() {
-	// trying to add autocomplete TODO: doesn't seem to work
-    if err := loadTaskLists(); err != nil {
-        log.Printf("Warning: Could not load task lists for autocomplete: %v", err)
+    app, err := NewApp()
+    if err != nil {
+        log.Fatal("Failed to initialize app:", err)
     }
 
-    filter := &backend.TaskFilter{}
-    var listName string
-
-    var rootCmd = &cobra.Command{
-        Use:   "gosynctasks",
-        Short: "Task synchronization tool",
+    rootCmd := &cobra.Command{
+        Use:               "gosynctasks [list-name]",
+        Short:             "Task synchronization tool",
+        Args:              cobra.MaximumNArgs(1),
+        ValidArgsFunction: app.listNameCompletion,
+        RunE:              app.run,
     }
 
-    var listCmd = &cobra.Command{
-        Use:   "list [list-name]",
-        Aliases: []string{"l"},
-        Short: "select task list",
-        Args:  cobra.ExactArgs(1),
-        ValidArgsFunction: listNameCompletion,
-        Run: func(cmd *cobra.Command, args []string) {
-            listName = args[0]
-        },
-    }
-
-    var statusCmd = &cobra.Command{
-        Use:     "status",
-        Aliases: []string{"s"},
-        Short:   "filter task by status",
-        Run: func(cmd *cobra.Command, args []string) {
-            var statuses []string
-
-            done, _ := cmd.Flags().GetBool("done")
-            todo, _ := cmd.Flags().GetBool("todo")
-            processing, _ := cmd.Flags().GetBool("processing")
-            cancelled, _ := cmd.Flags().GetBool("cancelled")
-
-            if done {
-                statuses = append(statuses, "DONE")
-            }
-            if todo {
-                statuses = append(statuses, "TODO")
-            }
-            if processing {
-                statuses = append(statuses, "PROCESSING")
-            }
-            if cancelled {
-                statuses = append(statuses, "CANCELLED")
-            }
-
-            if len(statuses) > 0 {
-                filter.Statuses = &statuses
-            }
-        },
-    }
-
-    statusCmd.Flags().BoolP("done", "d", false, "filter done tasks")
-    statusCmd.Flags().BoolP("todo", "t", false, "filter todo tasks")
-    statusCmd.Flags().BoolP("processing", "p", false, "filter processing tasks")
-    statusCmd.Flags().BoolP("cancelled", "c", false, "filter cancelled tasks")
-
-    rootCmd.AddCommand(listCmd)
-    rootCmd.AddCommand(statusCmd)
+    rootCmd.Flags().BoolP("done", "d", false, "filter done tasks")
+    rootCmd.Flags().BoolP("todo", "t", false, "filter todo tasks")
+    rootCmd.Flags().BoolP("processing", "p", false, "filter processing tasks")
+    rootCmd.Flags().BoolP("cancelled", "c", false, "filter cancelled tasks")
 
     if err := rootCmd.Execute(); err != nil {
         log.Fatal(err)
     }
-
-    // Main logic
-    config := config.GetConfig()
-    taskManager, err := config.Connector.TaskManager()
-    if err != nil {
-        log.Fatalln("There was a problem with connector's task manager.")
-    }
-
-    // If taskLists not loaded yet, load them now
-    if len(taskLists) == 0 {
-        taskLists, err = taskManager.GetTaskLists()
-        if err != nil {
-            fmt.Printf("there was an error while retrieving task lists: %s", err)
-            return
-        }
-    }
-
-    var selectedList *backend.TaskList
-
-    if listName != "" {
-        selectedList = findListByName(listName)
-        if selectedList == nil {
-            fmt.Printf("List '%s' not found\n", listName)
-            return
-        }
-    } else {
-        selectedList, err = cli_ListSelection(&taskLists)
-        if err != nil {
-            fmt.Printf("error: %s\n", err)
-            return
-        }
-    }
-
-    fmt.Println(selectedList)
-    tasks, err := taskManager.GetTasks(selectedList.ID, filter)
-    if err != nil {
-        fmt.Println("There was an error while retrieving tasks:")
-        fmt.Println(err)
-    }
-    fmt.Println(tasks)
-
-    fmt.Println(*config)
-    fmt.Println("End.")
 }
